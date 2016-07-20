cba73c96cb73addac3dd9a17ce79ca4579e18ff7
Vitalii Kalchuk <v.sweak@gmail.com>
2016-07-20 12:02:18 +0300

    updated gulp tasks and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # angular-table
+
+Clone repo
+
+$cd angular-table
+
+$npm install
+
+$gulp
+
+Open http://localhost:8888/ in your browser
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp'),
 	del = require('del'),
-	minifycss = require('gulp-minify-css'),
-	rev = require('gulp-rev'),
 	ngannotate = require('gulp-ng-annotate'),
 	uglify = require('gulp-uglify'),
-	usemin = require('gulp-usemin'),
-	minifyHtml = require('gulp-minify-html'),
 	less = require('gulp-less'),
 	cssmin = require('gulp-cssmin'),
-	htmlmin = require('gulp-htmlmin');
+	htmlmin = require('gulp-htmlmin'),
+    connect = require('gulp-connect'),
+    jsonminify = require('gulp-jsonminify');
     
 gulp.task('clean', function() {
     return del(['dest/*'], { force: true } );
@@ -18,22 +16,53 @@ gulp.task('less', function(){
     return gulp.src('./app/styles.less')
         .pipe(less())
 		.pipe(cssmin())
-        .pipe(gulp.dest('./dest'));
+        .pipe(gulp.dest('./dest'))
+        .pipe(connect.reload());
 });
 
 gulp.task('js', function () {
-  gulp.src(['./app/app.js'])
+  gulp.src(['./app/app.js', './node_modules/angular/angular.min.js'])
   	.pipe(ngannotate())
     .pipe(uglify())
-    .pipe(gulp.dest('./dest'));
+    .pipe(gulp.dest('./dest'))
+    .pipe(connect.reload());
 });
 
 gulp.task('html', function() {
   return gulp.src('./app/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./dest'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+    gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css ')
+        .pipe(cssmin())
+        .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('json', function () {
+    return gulp.src(['./app/data.json'])
+        .pipe(jsonminify())
+        .pipe(gulp.dest('./dest'))
+        .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 8888,
+    root: 'dest',
+    livereload: true
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+    gulp.watch(['./app/app.js'], ['js']);
+    gulp.watch(['./app/styles.less'], ['less']);
+    gulp.watch(['./app/data.json'], ['json']);
 });
 
 gulp.task('default', ['clean'], function() {
-    gulp.start('less', 'js');
+    gulp.start('less', 'js', 'html', 'css', 'json', 'connect', 'watch');
 });
